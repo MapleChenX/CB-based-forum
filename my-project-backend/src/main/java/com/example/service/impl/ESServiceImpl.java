923fec1764135a16cf409964017e240333de279b
maplechenx <maplechenonly@gmail.com>
2025-03-22 11:03:56 +0800

    推荐接口v2 mid

--- my-project-backend/src/main/java/com/example/service/impl/ESServiceImpl.java
+++ my-project-backend/src/main/java/com/example/service/impl/ESServiceImpl.java
@@ -4,16 +4,19 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.GetRequest;
 import co.elastic.clients.elasticsearch.core.GetResponse;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.SearchTemplateRequest;
 import com.alibaba.fastjson2.JSONObject;
 import com.example.common.Const;
 import com.example.entity.ESPostVector;
 import com.example.service.ESService;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
+import org.apache.commons.math3.stat.descriptive.summary.Product;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -21,6 +24,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class ESServiceImpl implements ESService {
+
+    // indices index get search update delete
 
     @Resource
     private ElasticsearchClient client;
@@ -49,27 +54,6 @@ public class ESServiceImpl implements ESService {
 //        client.indices().delete(d -> d.index(Const.ES_INDEX_FORUM_POSTS));  // 删除索引
 //        createIndex();  // 重新创建索引
         createIndexIfNotExists();  // 重新创建索引
-    }
-
-    void createIndex() throws IOException {
-        boolean exists = client.indices().exists(e -> e.index(Const.ES_INDEX_FORUM_POSTS)).value();
-        if (!exists) {
-            client.indices().create(c -> c
-                    .index(Const.ES_INDEX_FORUM_POSTS)
-                    .mappings(m -> m
-                            .properties("title", p -> p.text(t -> t))
-                            .properties("content", p -> p.text(t -> t))
-                            .properties("embedding", p -> p
-                                    .denseVector(d -> d
-                                            .dims(768)  // 向量维度
-                                    )
-                            )
-                    )
-            );
-            System.out.println("ES索引创建成功");
-        } else {
-            System.out.println("ES索引已存在，无需创建");
-        }
     }
 
     void createIndexIfNotExists() throws IOException {
@@ -112,19 +96,35 @@ public class ESServiceImpl implements ESService {
         }
     }
 
+    public void insertPostWithId(ESPostVector one)  {
+        try {
+            client.index(i -> i
+                    .index(Const.ES_INDEX_FORUM_POSTS)
+                    .id(one.getId())
+                    .document(one)
+            );
+            System.out.println("ES插入成功，id：" + one.getId());
+        }catch (Exception e) {
+            System.out.println("插入ES失败：" + e.getMessage());
+        }
+    }
+
+
     @Override
     public List<String> getSimilarPostsById(String id) {
-        // 获取指定 ID 的帖子向量
-        GetRequest req = new GetRequest.Builder().index(Const.ES_INDEX_FORUM_POSTS).id(id).build();
         try {
-            GetResponse<ESPostVector> esPostVectorGetResponse = client.get(req, ESPostVector.class);
-            if (esPostVectorGetResponse.source() == null)
-                return null;
-            List<Double> vector = esPostVectorGetResponse.source().getEmbedding();
-            if (vector == null || vector.isEmpty())
-                return null;
+            GetResponse<ESPostVector> response = client.get(g -> g
+                            .index(Const.ES_INDEX_FORUM_POSTS)
+                            .id(id),
+                    ESPostVector.class
+            );
 
-            // 执行 script_score 查询计算余弦相似度
+            if (!response.found()) {
+                return null;
+            }
+
+            List<Double> vector = response.source().getEmbedding();
+
             SearchResponse<ESPostVector> response = client.search(s -> s
                             .index(Const.ES_INDEX_FORUM_POSTS)
                             .query(q -> q
@@ -138,7 +138,7 @@ public class ESServiceImpl implements ESService {
                             )
                             .size(10)
                             .sourceFields("id"),
-                            ESPostVector.class
+                    ESPostVector.class
             );
 
             return response.hits().hits().stream()
@@ -150,11 +150,28 @@ public class ESServiceImpl implements ESService {
                     })
                     .filter(Objects::nonNull)
                     .collect(Collectors.toList());
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return null;
     }
+
+    // 搜索
+//    String searchText = "bike";
+//
+//    SearchResponse<Product> response = esClient.search(s -> s
+//                    .index("products")
+//                    .query(q -> q
+//                            .match(t -> t
+//                                    .field("name")
+//                                    .query(searchText)
+//                            )
+//                    ),
+//            Product.class
+//    );
 
 
     public boolean isExists(String id) throws IOException {
